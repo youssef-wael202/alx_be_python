7bbbaa531e204ff4589864b1643d1f35a270b754
youssef wael <yoyowael653@gmail.com>
2025-09-11 05:17:48 +0300

    make a simple calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#I is the interest
+#P is the principal amount
+#R is the rate of interest
+#T is the time in years
+principal = 1000    
+rate = 0.05
+time = 3
+interest = principal * time * rate
+print ("the simple interest is:", interest)
